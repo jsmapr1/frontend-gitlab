d1b50f8391424c15edadb418d9b36993393c07c3
jsmapr1 <joe.morgan@ku.edu>
2016-05-13 14:46:07 -0500

    add first test #2

--- src/requests/Issues.js
+++ src/requests/Issues.js
@@ -1,7 +1,13 @@
-const Issues = {
-  getIssues: () => {
-    return; 
-  }
+function generateRequestParameters(token) {
+  return {headers: new Headers({'PRIVATE-TOKEN': token})}
+};
+function validate(url,token) {
+  if(!token || !url) throw new Error("Please include Token");
 }
-
-export default Issues;
+export const  getIssues = ({url, token}) => {
+  validate(url,token);
+  return fetch(url+'/api/v3/issues', generateRequestParameters(token))
+    .then(response => {
+        return response.json();
+    })
+}
---
+++ test/requests/issues--test.js
@@ -0,0 +1,48 @@
+import expect from 'expect';
+import nock from 'nock';
+import fetch from 'isomorphic-fetch';
+import {getIssues} from '../../src/requests/Issues';
+
+describe('issues', () => {
+  const issue =  {
+    "id": 0,
+    "iid": 0,
+    "project_id": 0,
+    "title": "foo",
+    "description": "",
+    "state": "opened",
+    "created_at": "2016-05-12T08:16:27.337-05:00",
+    "updated_at": "2016-05-12T08:16:27.337-05:00",
+    "labels": [0],
+    "milestone": null,
+    "assignee": null,
+    "author": {
+      "name": "bar",
+      "username": "var",
+      "id": 1,
+      "state": "active",
+      "avatar_url": null,
+      "web_url": null,
+    },
+    "subscribed": true,
+  }
+  before(() => {
+    nock('http://foo.gitlab.com/api/v3', {
+        reqheaders: {
+          'PRIVATE-TOKEN': 'abc123'
+        }
+      })
+      .get('/issues')
+      .reply(200, [issue])
+  });
+
+  after (() => {
+    nock.cleanAll()
+  });
+
+  it('will work', () => {
+    return getIssues({url:'http://foo.gitlab.com', token:'abc123'}).then(json => {
+      expect(json).toEqual([issue])
+    });
+  })
+})
