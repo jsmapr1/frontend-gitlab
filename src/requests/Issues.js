07a4a7b9aeee9d82ae5c63743547181fd62e9284
jsmapr1 <joe.morgan@ku.edu>
2016-06-16 12:59:51 -0500

    project issue function stub and test #4

--- src/requests/Issues.js
+++ src/requests/Issues.js
@@ -1,13 +1,25 @@
 function generateRequestParameters(token) {
   return {headers: new Headers({'PRIVATE-TOKEN': token})}
 };
+
 function validate(url,token) {
   if(!token || !url) throw new Error("Please include Token");
 }
-export const  getIssues = ({url, token}) => {
+
+export const getIssues = ({url, token}) => {
   validate(url,token);
   return () => {
-    return fetch(url+'/api/v3/issues', generateRequestParameters(token))
+    return fetch(url + '/api/v3/issues', generateRequestParameters(token))
+      .then(response => {
+          return response.json();
+      })
+  }
+}
+
+export const  getProjectIssues = ({url, token}) => {
+  validate(url,token);
+  return (project, options) => {
+    return fetch(url + '/api/v3/projects/0/issues?state=closed', generateRequestParameters(token))
       .then(response => {
           return response.json();
       })
--- test/requests/issues--test.js
+++ test/requests/issues--test.js
@@ -1,7 +1,6 @@
 import expect from 'expect';
 import nock from 'nock';
-import fetch from 'isomorphic-fetch';
-import {getIssues} from '../../src/requests/Issues';
+import {getIssues, getProjectIssues} from '../../src/requests/Issues';
 
 describe('issues', () => {
   const issue =  {
@@ -26,6 +25,30 @@ describe('issues', () => {
     },
     "subscribed": true,
   }
+
+  const issue2 =  {
+    "id": 1,
+    "iid": 1,
+    "project_id": 0,
+    "title": "bar",
+    "description": "",
+    "state": "closed",
+    "created_at": "2016-05-12T08:16:27.337-05:00",
+    "updated_at": "2016-05-12T08:16:27.337-05:00",
+    "labels": [0],
+    "milestone": null,
+    "assignee": null,
+    "author": {
+      "name": "bar",
+      "username": "var",
+      "id": 1,
+      "state": "active",
+      "avatar_url": null,
+      "web_url": null,
+    },
+    "subscribed": true,
+  }
+
   before(() => {
     nock('http://foo.gitlab.com/api/v3', {
         reqheaders: {
@@ -34,7 +57,15 @@ describe('issues', () => {
       })
       .persist()
       .get('/issues')
-      .reply(200, [issue])
+      .reply(200, [issue, issue2])
+
+    nock('http://foo.gitlab.com/api/v3', {
+        reqheaders: {
+          'PRIVATE-TOKEN': 'abc123'
+        }
+      })
+      .get('/projects/0/issues?state=closed')
+      .reply(200, [issue2])
   });
 
   after (() => {
@@ -43,14 +74,22 @@ describe('issues', () => {
 
   it('will return issues', () => {
     return getIssues({url:'http://foo.gitlab.com', token:'abc123'})().then(json => {
-      expect(json).toEqual([issue])
+      expect(json).toEqual([issue, issue2])
     });
   })
 
   it('can take authentication separate', () => {
     const issues = getIssues({url:'http://foo.gitlab.com', token:'abc123'});
     return issues().then(json => {
-      expect(json).toEqual([issue])
+      expect(json).toEqual([issue, issue2])
     });
+  })
+
+
+  it('can return issues for a project', () => {
+    return getProjectIssues({url:'http://foo.gitlab.com', token:'abc123'})(0, {})
+      .then(json => {
+        expect(json).toEqual([issue2])
+      });
   })
 })
