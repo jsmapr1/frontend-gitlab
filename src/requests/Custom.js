16f98a612f9d72f935ca3fcb0f91de1b0b6e0cec
jsmapr1 <joe.morgan@ku.edu>
2016-08-02 08:37:09 -0500

    removed random semicolon #3

--- src/requests/Custom.js
+++ src/requests/Custom.js
@@ -5,7 +5,6 @@ const BASE_URL = '/api/v3/'
 export const request = ({url, token}) => {
   validate(url,token);
   return (path, {requestType, params}) => {
-    ;
     let parametized = params && Object.keys(params).length ? parametize(params):'';
     return fetch(url + BASE_URL + path + parametized,
       Object.assign(
