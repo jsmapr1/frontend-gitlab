eff65557ecf7ebf6be9a32bd9f47f2091704f766
jsmapr1 <joe.morgan@ku.edu>
2016-08-17 10:26:57 -0500

    add default parameter of empty object for custom request

--- src/requests/Custom.js
+++ src/requests/Custom.js
@@ -4,7 +4,7 @@ const BASE_URL = '/api/v3/'
 
 export const request = ({url, token}) => {
   validate(url,token);
-  return (path, {requestType, params}) => {
+  return (path, {requestType, params} = {}) => {
     return fetch(url + BASE_URL + path + parametize(params),
       Object.assign(
         generateRequestParameters(token),
